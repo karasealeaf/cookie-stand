c22b362b90e601084e8532245d211e773f86ca52
kara sealeaf <kara.sealeaf13@gmail.com>
2023-09-25 14:15:24 +0100

    sales

---
+++ app.js
@@ -0,0 +1,43 @@
+console.log("Hello World");
+
+const shop = {
+  name: ['Seattle', 'Tokyo','Dubai','Paris','Lima'],
+  hours: [6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],
+  max: [16,38,24,65],
+  min:[2,3,11,20,23]
+  
+  avgCookies: function(avgCookies) {
+for (i = 0; i < hours.length; i++) {
+  this.avgCookies = [i];
+}
+  }
+}
+
+function generateRandom(maxLimit = 100){
+  let rand = Math. random() * maxLimit;
+  console. log(rand); // say 99.81321410836433.
+  ​
+  rand = Math. floor(rand); // 99.
+  ​
+  return rand;
+  }
+
+Objects/Math/random{:target="_blank"};
+
+// Seattle
+
+// 6am: 16 cookies
+// 7am: 20 cookies
+// 8am: 35 cookies
+// 9am: 48 cookies
+// 10am: 56 cookies
+// 11am: 77 cookies
+// 12pm: 93 cookies
+// 1pm: 144 cookies
+// 2pm: 119 cookies
+// 3pm: 84 cookies
+// 4pm: 61 cookies
+// 5pm: 23 cookies
+// 6pm: 42 cookies
+// 7pm: 57 cookies
+// Total: 875 cookies

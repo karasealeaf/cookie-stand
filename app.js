a6064bd855cf2409a7dd60db2a5561297201e15e
kara sealeaf <kara.sealeaf13@gmail.com>
2023-09-25 16:06:37 +0100

    ul added

--- app.js
+++ app.js
@@ -14,9 +14,11 @@ const hours = [
   "6pm",
   "7pm",
 ];
+
 function randomNumber(min, max) {
-  return Math.floor(math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
 const seattle = {
   location: "seattle",
   minCust: 23,
@@ -25,8 +27,7 @@ const seattle = {
   customersPerHour: [],
   cookiesPerHour: [],
   totalCookieSold: 0,
-  calcCustPerHour: function () {
-    // add a random number to the customersPerHour array
+  calculateSales: function () {
     for (let i = 0; i < hours.length; i++) {
       const randNum = randomNumber(this.minCust, this.maxCust);
       this.customersPerHour.push(randNum);
@@ -34,49 +35,26 @@ const seattle = {
     }
   },
 };
-seattle.calcCustPerHour();
+
+seattle.calculateSales();
 console.log(seattle);
 
-// console.log("Hello World");
+const salesData = document.getElementById("salesData");
 
-// const shop = {
-//   name: ['Seattle', 'Tokyo','Dubai','Paris','Lima'],
-//   hours: [6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],
-//   max: [16,38,24,65],
-//   min:[2,3,11,20,23]
+const article = document.createElement("article");
 
-//   avgCookies: function(avgCookies) {
-// for (i = 0; i < hours.length; i++) {
-//   this.avgCookies = [i];
-// }
-//   }
-// }
+const h2 = document.createElement("h2");
+h2.textContent = seattle.calculateSales;
+article.appendChild(h2);
 
-// function generateRandom(maxLimit = 100){
-//   let rand = Math. random() * maxLimit;
-//   console. log(rand); // say 99.81321410836433.
-//   ​
-//   rand = Math. floor(rand); // 99.
-//   ​
-//   return rand;
-//   }
+const p = document.createElement("p");
+p.textContent = `${seattle.calculateSales} is ${seattle.cookiesPerHour} avergeing ${seattle.avgCookiesPerCust}`;
+article.appendChild(p);
 
-// Objects/Math/random{:target="_blank"};
-
-// Seattle
-
-// 6am: 16 cookies
-// 7am: 20 cookies
-// 8am: 35 cookies
-// 9am: 48 cookies
-// 10am: 56 cookies
-// 11am: 77 cookies
-// 12pm: 93 cookies
-// 1pm: 144 cookies
-// 2pm: 119 cookies
-// 3pm: 84 cookies
-// 4pm: 61 cookies
-// 5pm: 23 cookies
-// 6pm: 42 cookies
-// 7pm: 57 cookies
-// Total: 875 cookies
+const ul = document.createElement("ul");
+for (let i = 0; i < seattle.calculateSales.length; i++) {
+  const li = document.createElement("li");
+  li.textContent = seattle.calculateSales[i];
+  ul.appendChild(li);
+}
+article.appendChild(ul);
